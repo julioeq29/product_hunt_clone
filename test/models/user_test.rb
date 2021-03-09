1d0501f1ee67692464c536f4833feb4830539730
Julio Quintana <julioeq29@gmail.com>
2021-03-09 18:26:36 +0000

    Prepping Travis

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "john", last_name: "lennon")
+    # order is -> expected // actual
+    assert_equal "John Lennon", user.full_name
+  end
 end
